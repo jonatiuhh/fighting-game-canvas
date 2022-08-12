6f91a66ccee09b6675abcfb22df547d573eddaef
Jonathan Sesma <108715533+jonatiuhh@users.noreply.github.com>
2022-08-12 17:54:10 -0500

    .

---
+++ index.js
@@ -0,0 +1,56 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+  constructor({position, velocity}) {
+    this.position = position;
+  }
+
+  draw() {
+    c.fillStyle = 'red';
+    c.fillRect(this.position.x, this.position.y, 50, 150)
+  }
+
+  update(){
+    
+  }
+}
+
+const player = new Sprite({
+  position: {
+    x: 0,
+    y: 0
+  }, 
+  velocity: {
+    x: 0,
+    y: 0
+  }
+})
+
+player.draw();
+
+const enemy = new Sprite({
+  position: {
+    x: 400,
+    y: 100
+  }, 
+  velocity: {
+    x: 0,
+    y: 0
+  }
+})
+
+enemy.draw();
+
+console.log(player);
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  console.log('go');
+}
+
