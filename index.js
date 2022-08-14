7e3ee9fc90012567cf898c40e71cec68a09dd07a
Jonathan Sesma <108715533+jonatiuhh@users.noreply.github.com>
2022-08-13 22:36:09 -0500

    .

--- index.js
+++ index.js
@@ -22,10 +22,13 @@ class Sprite {
 
   update(){
     this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
     if (this.position.y + this.height + this.velocity.y >= canvas.height){
       this.velocity.y = 0;
+    }else{
+      this.velocity.y += gravity;
     }
   }
 }
@@ -37,7 +40,7 @@ const player = new Sprite({
   }, 
   velocity: {
     x: 0,
-    y: 10
+    y: 0
   }
 })
 
@@ -55,8 +58,21 @@ const enemy = new Sprite({
 })
 
 
-
 console.log(player);
+
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+  w: {
+    pressed: false
+  }
+}
+
+let lastkey;
 
 function animate() {
   window.requestAnimationFrame(animate);
@@ -64,6 +80,45 @@ function animate() {
   c.fillRect(0,0, canvas.width, canvas.height);
   player.update();
   enemy.update();
+
+  player.velocity.x = 0;
+
+  if (keys.a.pressed && lastkey === 'a') {
+    player.velocity.x = -1;
+  } else if (keys.d.pressed && lastkey === 'd') {
+    player.velocity.x = 1;
+  }
 }
 
+
 animate();
+
+window.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = true;
+      lastkey = 'd';
+      break;
+    case 'a':
+      keys.a.pressed = true;
+      lastkey = 'a';
+      break;
+    case 'w':
+      player.velocity.y = -10
+      break;
+  }
+})
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = false;
+      break;
+    case 'a':
+      keys.a.pressed = false;
+      break;
+    case 'w':
+      keys.w.pressed = false;
+      break;
+  }
+})
